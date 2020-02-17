9cadb43a0a57c7d5b0d2b0cecb19903f51ab6efe
Bryan DiLaura <dilauraba@gmail.com>
2020-02-16 21:50:55 -0700

    more hard math

--- formula.py
+++ formula.py
@@ -7,4 +7,15 @@ def coin_flip(n):
 
     return float(((coin_flip(n-1) + coin_flip(n-2)) / 2) + 1)
 
-print(coin_flip(10))
+def coin_flip_fuck_recursion(n):
+    
+    step = 2
+    answers = [2.0, 2.0]
+    while(step != n):
+        answers.append(((answers[step-1] + answers[step-2]) / 2) + 1)
+        step += 1 
+
+    return answers[n-1]
+
+# print(coin_flip(30))
+print(coin_flip_fuck_recursion(100))
